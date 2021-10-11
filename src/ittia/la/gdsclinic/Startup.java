dcf904c6f444fee2b28b2b4914cf1424af17b998
MigoJJ <jjkoh21@gmail.comgit config user.namegit config --global user.email  jjkoh21@gmail.com>
2021-10-10 18:24:47 -0700

    ittia 20211011 시작

---
+++ src/ittia/la/gdsclinic/Startup.java
@@ -0,0 +1,11 @@
+package ittia.la.gdsclinic;
+
+public class Startup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub		// TODO Auto-generated method stub		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ittia/nu/gdsclinic/Startup.java
@@ -0,0 +1,10 @@
+package ittia.nu.gdsclinic;
+
+public class Startup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
