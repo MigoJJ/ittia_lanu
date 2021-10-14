252370e21dc7436a83099f93f1d97c7368b12914
MigoJJ <jjkoh21@gmail.comgit config user.namegit config --global user.email  jjkoh21@gmail.com>
2021-10-13 22:49:41 -0700

    ittia 20211014

--- src/ittia/la/gdsclinic/Startup.java
+++ src/ittia/la/gdsclinic/Startup.java
@@ -1,18 +1,15 @@
 package ittia.la.gdsclinic;
+import ittia.nu.gdsclinic.CurrentDate;
+import ittia.nu.gdsclinic.FileReading;
 
 public class Startup {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub		// TODO Auto-generated method stub		// TODO Auto-generated method stub
+		
+		CurrentDate.c_time();
+		
+		FileReading.startread();
 		
 		
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub		// TODO Auto-generated method stub		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub		// TODO Auto-generated method stub		// TODO Auto-generated method stub// TODO Auto-generated method stub
-		// TODO Auto-generated method stub		// TODO Auto-generated method stub		// TODO Auto-generated method stub
-
 	}
 
 }
---
+++ src/ittia/nu/gdsclinic/CurrentDate.java
@@ -0,0 +1,14 @@
+package ittia.nu.gdsclinic;
+import java.util.*;
+
+public class CurrentDate {
+		    public static void c_time()
+		    {
+		        Date current_Date = new Date();
+		        //"Date" class
+		        //"current_Date" is Date object
+		  
+		        System.out.println(current_Date);
+		        // print the time and date
+		    }
+	}
