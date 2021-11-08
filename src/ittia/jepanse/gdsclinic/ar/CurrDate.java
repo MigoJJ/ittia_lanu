1d8471941583e54fa723775d9a26371fa96e4a1a
MigoJJ <63824334+MigoJJ@users.noreply.github.com>
2021-11-09 08:02:17 +0900

    Update CurrDate.java

--- src/ittia/jepanse/gdsclinic/ar/CurrDate.java
+++ src/ittia/jepanse/gdsclinic/ar/CurrDate.java
@@ -3,12 +3,11 @@ package ittia.jepanse.gdsclinic.ar;
 import java.util.*;
 import java.text.*;
 
-public class CurrDate {
-		public static void c_time() {
-		      Date dNow = new Date( );
-		      SimpleDateFormat ft = 
-		      new SimpleDateFormat ("E'요일'  yyyy.MM.dd 'at' hh:mm:ss a zzz");
-	
-		      System.out.println("Current Date: " + ft.format(dNow));
+	public class CurrDate {
+	public static void c_time() {
+	      Date dNow = new Date( );
+	      SimpleDateFormat ft = 
+	      new SimpleDateFormat ("E'요일'  yyyy.MM.dd 'at' hh:mm:ss a zzz");
+	      System.out.println("Current Date: " + ft.format(dNow));
    }
 }
