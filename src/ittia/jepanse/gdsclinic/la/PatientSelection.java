19bfb9206c10b41fd7c53107dfaee61af327887c
Migo JJ <jjkoh21@gmail.com>
2021-11-10 22:00:01 +0900

    Entered code is  :

--- src/ittia/jepanse/gdsclinic/la/PatientSelection.java
+++ src/ittia/jepanse/gdsclinic/la/PatientSelection.java
@@ -12,6 +12,6 @@ public class PatientSelection {
 			
 			FileList.selfilelist(ptname);
 			
-//			System.out.println("Entered code is  :  " + ptname);
+			System.out.println("Entered code is  :  " + ptname);
 		}
 	}
